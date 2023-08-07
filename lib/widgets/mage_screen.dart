6f50d57bf6c3b506febc983a1658692403d7097d
keremErdine <keremerdine@hotmail.com>
2023-08-07 11:16:45 +0300

    modified:   lib/widgets/mage_screen.dart

--- lib/widgets/mage_screen.dart
+++ lib/widgets/mage_screen.dart
@@ -12,7 +12,38 @@ class MageScreen extends StatefulWidget {
 class _MageScreenState extends State<MageScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+            'BÜYÜCÜ',
+            style: Theme.of(context).textTheme.titleMedium!,
+          ),
+          Text(
+            'Büyücüler sana rastgele büyü kitapları satar. Eğer o kitabı beğenmediysen mana çıkarımı ile mana kazanabilir ya da birleştirip daha iyi kitaplar elde etme şansını alabilirsin.',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Row(
+            children: [
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.ac_unit_outlined),
+                label: const Text('ÖĞREN'),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.auto_fix_high_outlined),
+                label: const Text('MANA ÇIKARMA'),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.library_books_outlined),
+                label: const Text('BİRLEŞTİRME'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
